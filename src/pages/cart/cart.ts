e6de8fd192776083d914670adf295ce175cdf389
Vinicius Lima <viniciusb80@gmail.com>
2018-12-13 17:10:42 -0200

    Criando pagina de carrinho de compras

---
+++ src/pages/cart/cart.ts
@@ -0,0 +1,46 @@
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { CartItem } from "../../models/cart-item";
+import { ProdutoService } from "../../services/domain/produto.service";
+import { API_CONFIG } from "../../config/api.config";
+import { CartService } from "../../services/domain/cart.service";
+
+@IonicPage()
+@Component({
+  selector: "page-cart",
+  templateUrl: "cart.html"
+})
+export class CartPage {
+  items: CartItem[];
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public cartService: CartService,
+    public produtoService: ProdutoService
+  ) {}
+
+  ionViewDidLoad() {
+    let cart = this.cartService.getCart();
+    this.items = cart.items;
+    this.loadImageUrls();
+  }
+
+  loadImageUrls() {
+    for (let i = 0; i < this.items.length; i++) {
+      let item = this.items[i];
+      item.produto.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${
+        item.produto.id
+      }-small.jpg`; // Workaround for Python server
+      // this.produtoService.getSmallImageFromBucket(item.produto.id).subscribe(
+      //   response => {
+      //     console.log(response);
+      //     item.produto.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${
+      //       item.produto.id
+      //     }-small.jpg`;
+      //   },
+      //   error => {}
+      // );
+    }
+  }
+}
