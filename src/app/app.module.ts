cec2d65f36ab0f9beed5b421007d7e620cdb899d
Vinicius Lima <viniciusb80@gmail.com>
2018-12-17 11:40:44 -0200

    Fazendo upload da foto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ClienteService } from "../services/domain/cliente.service";
 import { AuthInterceptorProvider } from "../interceptors/auth-interceptor";
 import { ProdutoService } from "../services/domain/produto.service";
 import { CartService } from "../services/domain/cart.service";
+import { ImageUtilService } from "../services/image-utils.service";
 
 @NgModule({
   declarations: [MyApp],
@@ -32,7 +33,8 @@ import { CartService } from "../services/domain/cart.service";
     StorageService,
     ClienteService,
     ProdutoService,
-    CartService
+    CartService,
+    ImageUtilService
   ]
 })
 export class AppModule {}
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -4,10 +4,15 @@ import { Observable } from "rxjs";
 import { ClienteDTO } from "../../models/cliente.dto";
 import { API_CONFIG } from "../../config/api.config";
 import { StorageService } from "../storage.service";
+import { ImageUtilService } from "../image-utils.service";
 
 @Injectable()
 export class ClienteService {
-  constructor(public http: HttpClient, public storage: StorageService) {}
+  constructor(
+    public http: HttpClient,
+    public storage: StorageService,
+    public imageUtilService: ImageUtilService
+  ) {}
 
   findById(id: string) {
     return this.http.get(`${API_CONFIG.baseUrl}/clientes/${id}`);
@@ -24,6 +29,16 @@ export class ClienteService {
 
   insert(obj: ClienteDTO) {
     return this.http.post(`${API_CONFIG.baseUrl}/clientes`, obj, {
+      observe: "response",
+      responseType: "text"
+    });
+  }
+
+  uploadPicture(picture) {
+    let pictureBlog = this.imageUtilService.dataUriToBlob(picture);
+    let formData: FormData = new FormData();
+    formData.set("file", pictureBlog, "file.png");
+    return this.http.post(`${API_CONFIG.baseUrl}/clientes/picture`, formData, {
       observe: "response",
       responseType: "text"
     });
